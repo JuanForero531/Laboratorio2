6b66f05db2d4aa63dfd8e9117e8f05e5801881ec
JuanForero531 <111099370+JuanForero531@users.noreply.github.com>
2024-10-16 12:21:32 -0500

    Update server.js

--- middleware/server.js
+++ middleware/server.js
@@ -3,8 +3,10 @@ const axios = require('axios');
 const cors = require('cors');
 const multer = require('multer');
 const app = express();
+const path = require('path');
 const port = 4000;
 
+app.use('/output', express.static(path.join(__dirname, 'output')));
 app.use(express.json());
 app.use(cors());
 const upload = multer();
@@ -13,20 +15,20 @@ const upload = multer();
 let connections = [];
 const logs = [];
 
-
 app.post("/middleware", (req, res) => {
-  let instance = req.body
-  connections.push({instance:`${instance.ipAddress}:${instance.port}`, requests: 0 , logs: [], tried:false });
+  let instance = req.body;
+  console.log("Datos recibidos de la instancia:", instance);
+  connections.push({instance: `${instance.ipAddress}:${instance.port}`, requests: 0, logs: [], tried: false });
   res.status(200).end();
   console.log("Instancias actualizadas: ", connections);
 });
 
-app.post("/request", upload.single('image'), async (req, res) => {
+const FormData = require('form-data');
 
+app.post("/request", upload.single('image'), async (req, res) => {
   if (connections.length === 0) {
     return res.status(503).json({ error: "No hay servidores disponibles" });
   }
-
 
   let leastConnectedServer = connections.reduce((prev, curr) => 
     prev.requests < curr.requests ? prev : curr
@@ -34,25 +36,25 @@ app.post("/request", upload.single('image'), async (req, res) => {
 
   for (let i = 0; i < connections.length; i++) {
     try {
-
       const formData = new FormData();
       formData.append('image', req.file.buffer, req.file.originalname);
 
       console.log(`Llamando a servidor: ${leastConnectedServer.instance}/upload`);
-      const response = await axios.post(`http://${leastConnectedServer.instance}/upload`, formData,{
-         headers: {
-          ...formData.getHeaders(),
-         }
+      
+      const response = await axios.post(`http://${leastConnectedServer.instance}/upload`, formData, {
+        headers: formData.getHeaders(),
       });
+
       leastConnectedServer.requests++;
-      leastConnectedServer.logs.push(`${new Date()} - ${req.originalUrl} -  ${req.method}. ${JSON.stringify(response.data)}`)
+      leastConnectedServer.logs.push(`${new Date()} - ${req.originalUrl} -  ${req.method}. ${JSON.stringify(response.data)}`);
 
       res.status(response.status).send(response.data);
+      return;
 
     } catch (error) {
-
       console.log(`Error. ${leastConnectedServer.instance}: ${error.message}. ${new Date()}`);
       leastConnectedServer.requests++;
+
       if (error.response) {
         leastConnectedServer.logs.push(`${new Date()} - ${error.response.status} - ${error.message}`);
       } else {
@@ -61,22 +63,19 @@ app.post("/request", upload.single('image'), async (req, res) => {
       leastConnectedServer.tried = true;
 
       const availableConnections = connections.filter(conn => !conn.tried);
-      
       if (availableConnections.length > 0) {
         leastConnectedServer = availableConnections.reduce((prev, curr) => 
           prev.requests < curr.requests ? prev : curr
         );
-        console.log(leastConnectedServer)
+        console.log(leastConnectedServer);
       } else {
         connections.forEach(conn => conn.tried = false);
         return res.status(503).json({ error: "No hay servidores disponibles" });
-
       }
     }
   }
 
   connections.forEach(conn => conn.tried = false);
-
 });
 
 
